c41569f55bfc6029efd5df8c4b95eed1beb13f34
jkest90 <jkest618@gmail.com>
2019-05-20 21:05:03 -0600

    react  & heroku

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Static route initialized.')
-});
+// router.get('/', (req, res, next) => {
+//   res.send('Static route initialized.')
+// });
 
 router.get('/api/customers', (req, res) => {
   const customers = [
