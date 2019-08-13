53e843738cf22a1a4b09127fe26a71a1b435b7bf
jkest90 <jkest618@gmail.com>
2019-08-13 16:17:42 -0600

    fixed navbar

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -7,44 +7,48 @@ import IosArrowDropdown from 'react-ionicons/lib/IosArrowDropdown';
 
 
 const Landing = (props) => {
-  return (
-    <div className='container'>
-        <div className='row'>
-          <div className='col-lg-12' id='heading-text' >
-             <h1 className='header text-dark' > <strong> Simple. Intuitive. <em>Easy.</em>  </strong></h1>
-             <h1 className='text-dark mt-4'> <strong> Bookmarking done right.</strong> </h1>
-          </div>
-        </div>
-        <div className='row' id='heading-row'>
-          <div className='col-lg-4 col-sm-12 feature-column'>
-            <div className='card bg-dark border-dark' id='landingCards' >
-              <img className='card-img-top' src={folderIcon} alt='search icon'/>
-              <div className='card-body text-dark'>
-                <p className='card-text'>Quickly search through your collections to instantly find what you need.</p>
-              </div>
-            </div>
-          </div>
-          <div className='col-lg-4 col-sm-12 feature-column'>
-            <div className='card bg-dark border-dark ' id='landingCards'>
-              <img className='card-img-top' src={searchIcon} alt='tag icon'/>
-              <div className='card-body text-dark'>
-                <p className='card-text'>Add re-usable tags to your bookmarks to link resources in different folders.</p>
-              </div>
-            </div>
-          </div>
-          <div className='col-lg-4 col-sm-12 feature-column'>
-            <div className='card bg-dark border-dark' id='landingCards'>
-              <img className='card-img-top' src={tagIcon} alt='folder icon'/>
-               <div className='card-body text-dark'>
-                <p className='card-text'> Organize your bookmarks into customizable folders and access them easily with folder filtering. </p>
-              </div>
-            </div>
-          </div>
-      </div>
-    </div>
 
-        
+  return (
+
+    <div></div>
   )
 };
 
 export default Landing;
+
+
+{/* <div className='container'>
+  <div className='row'>
+    <div className='col-lg-12' id='heading-text'>
+      <h1 className='header text-dark'> <strong> Simple. Intuitive. <em>Easy.</em> </strong></h1>
+      <h1 className='text-dark mt-4'> <strong> Bookmarking done right.</strong> </h1>
+    </div>
+  </div>
+  <div className='row' id='heading-row'>
+    <div className='col-lg-4 col-sm-12 feature-column'>
+      <div className='card bg-dark border-dark' id='landingCards'>
+        <img className='card-img-top' src={folderIcon} alt='search icon' />
+        <div className='card-body text-dark'>
+          <p className='card-text'>Quickly search through your collections to instantly find what you need.</p>
+        </div>
+      </div>
+    </div>
+    <div className='col-lg-4 col-sm-12 feature-column'>
+      <div className='card bg-dark border-dark ' id='landingCards'>
+        <img className='card-img-top' src={searchIcon} alt='tag icon' />
+        <div className='card-body text-dark'>
+          <p className='card-text'>Add re-usable tags to your bookmarks to link resources in different folders.</p>
+        </div>
+      </div>
+    </div>
+    <div className='col-lg-4 col-sm-12 feature-column'>
+      <div className='card bg-dark border-dark' id='landingCards'>
+        <img className='card-img-top' src={tagIcon} alt='folder icon' />
+        <div className='card-body text-dark'>
+          <p className='card-text'> Organize your bookmarks into customizable folders and access them easily with folder
+            filtering. </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> */}
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 // import Bootstrap from 'bootstrap';
 // import axios from 'axios'; 
 
-import logo from '../assets/img/logo_transparent_white.png';
-import logoLogout from '../assets/img/logo_transparent.png';
+// import logo from '../assets/img/logo_transparent_white.png';
+import logoLogout from '../assets/img/logo_transparent_white.png';
 
  class NavBar extends Component {
   constructor(props) {
@@ -29,49 +29,63 @@ import logoLogout from '../assets/img/logo_transparent.png';
     // console.log(this.props);
     return (
 
+
+
       this.props.loggedIn ? (
- 
-        <nav className='navbar mr-3' id='navBar'>
-          <a href='/' className='navbar-brand'><img id='logoutLogo' src={logoLogout} alt='logo'/></a>
-          <Link to='/' className='nav-link mb-4 ml-4 text-primary btn btn-outline-primary bg-white btn-sm' onClick={this.props.signoutUser}> Log Out</Link>
-        </nav>
+        
+       <nav class="navbar navbar-expand-lg fixed-top ">
+         <a class="navbar-brand" href="/"><img id='logoutLogo' src={logoLogout} alt='logo' /></a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse " id="navbarSupportedContent">
+          <ul class="navbar-nav mr-4">
+            <li class="nav-item">
+              <Link to='/'class="nav-link" data-value="about" href="/" onClick={this.props.signoutUser}>Log Out</Link>
+            </li>
+          </ul>
+         </div>
+       </nav>
       
       ) : (
-
-      <nav className='navbar mr-3' id='navBar'> 
-        <a href='/bookmarks' className='navbar-brand mt-4'><img id='loginLogo' src={logo} alt='logo'/></a>
-        <Link to='/signin' className='nav-link mt-4 text-primary btn btn-primary bg-white landing-nav btn-lg' > Log In </Link>
-        <Link to='/signup' className='nav-link mt-4 ml-3 text-white btn bg-primary btn-lg'> Sign Up</Link>
-      </nav> 
-
+       <nav class="navbar navbar-expand-lg fixed-top ">
+         <a class="navbar-brand" href="/"><img id='logoutLogo' src={logoLogout} alt='logo' /></a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent"
+           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse " id="navbarSupportedContent">
+           <ul class="navbar-nav mr-4">
+             <li class="nav-item">
+               <Link to='/signup' class="nav-link" data-value="about" href="/">Sign Up</Link> 
+            </li>
+            <li class="nav-item">
+              <Link to='/signin' class="nav-link" data-value="portfolio" href="/">Log In</Link>
+            </li>
+            
+           </ul>
+         </div>
+       </nav>
       )
+
+      
     )
-  }
+} 
   
 }
 
 export default NavBar;
 
+// eslint-disable-next-line no-lone-blocks
+{/* <nav className='navbar mr-3' id='navBar'>
+  <a href='/' className='navbar-brand'><img id='logoutLogo' src={logoLogout} alt='logo' /></a>
+  <Link to='/' className='nav-link mb-4 ml-4 text-primary btn btn-outline-primary bg-white btn-sm'
+    onClick={this.props.signoutUser}> Log Out</Link>
+</nav>
 
-// <nav class="navbar navbar-expand-lg navbar-light bg-light">
-//   <img src={logo} />
-//   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//     <span class="navbar-toggler-icon"></span>
-//   </button>
-//   <div class="collapse navbar-collapse" id="navbarNav">
-//     <ul class="nav navbar-nav ml-auto">
-//       <li class="nav-item active">v
-//         <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-//       </li>
-//       <li class="nav-item">
-//         <a class="nav-link" href="/">Features</a>
-//       </li>
-//       <li class="nav-item">
-//         <a class="nav-link" href="/">Pricing</a>
-//       </li>
-//       <li class="nav-item">
-//         <a class="nav-link disabled" href="/">Disabled</a>
-//       </li>
-//     </ul>
-//     </div>
-//   </nav>
+<nav className='navbar mr-3' id='navBar'>
+  <a href='/bookmarks' className='navbar-brand mt-4'><img id='loginLogo' src={logo} alt='logo' /></a>
+  <Link to='/signin' className='nav-link mt-4 text-primary btn btn-primary bg-white landing-nav btn-lg'> Log In </Link>
+  <Link to='/signup' className='nav-link mt-4 ml-3 text-white btn bg-primary btn-lg'> Sign Up</Link>
+</nav> */}
