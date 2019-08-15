4a99f84b13d944b018e46a2c26643e1a9a4ab46e
jkest90 <jkest618@gmail.com>
2019-08-14 21:44:17 -0600

    fuckme

--- src/server.js
+++ src/server.js
@@ -27,8 +27,7 @@ server.listen(port);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
