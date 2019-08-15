fce07c14cdc06157ac4cf844f149457078a118db
jkest90 <jkest618@gmail.com>
2019-08-14 22:02:16 -0600

    help

--- src/server.js
+++ src/server.js
@@ -26,9 +26,16 @@ server.listen(port);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'app/client/built/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
 }
 
 server.on("listening", () => {
