6e314ac18c07fb285eedb17317c2bcef5d516669
jkest90 <jkest618@gmail.com>
2019-08-14 18:46:24 -0600

    fixed description datatype

--- src/db/migrations/20190530005653-create-bookmark.js
+++ src/db/migrations/20190530005653-create-bookmark.js
@@ -18,7 +18,7 @@ module.exports = {
       },
       description: {
         allowNull: false,
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
         defaultValue: 'This bookmark has no description.'
       },
       screenshot: {
